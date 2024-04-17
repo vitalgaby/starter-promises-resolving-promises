39c36a441bfa75c888fa472b79761d3bf936b7c9
Vitalgaby <gabsanchez18@outlook.com>
2024-04-17 11:19:15 -0700

    updated

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
-
 const promise = welcome();
-
 console.log(promise);
+const question = "Will the weather be nice today?";
+
+const tellPromise = tell(question);
+welcome()
+  .then(console.log) // Logs the result of welcome()
+  .then(tell) // Calls tell, which returns a rejected promise (no question supplied).
+  .then(console.log) // Skipped because tell returned a rejected promise.
+  .catch(console.error) // Logs error from tell() or welcome()
+  .then(goodbye) // Returns promise from goodbye()
+  .then(console.log) // Logs the result from goodbye()
+  .catch(console.error); // Logs error only from goodbye()

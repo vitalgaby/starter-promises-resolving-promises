8af07c305b3f63ce7dba6eaf3be430112ac79148
Vitalgaby <gabsanchez18@outlook.com>
2024-04-17 13:17:43 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-const { welcome, goodbye, tell } = require("../utils/fortune-teller");
-const promise = welcome();
-console.log(promise);
-const question = "Will the weather be nice today?";
+const axios = require("../utils/axios");
+const BASE_URL = "http://localhost:5000";
 
-const tellPromise = tell(question);
-welcome()
-  .then(console.log) // Logs the result of welcome()
-  .then(tell) // Calls tell, which returns a rejected promise (no question supplied).
-  .then(console.log) // Skipped because tell returned a rejected promise.
-  .catch(console.error) // Logs error from tell() or welcome()
-  .then(goodbye) // Returns promise from goodbye()
-  .then(console.log) // Logs the result from goodbye()
-  .catch(console.error); // Logs error only from goodbye()
+function getAllNames() {}
+
+function getConstellationsByQuadrant(quadrant) {}
+
+module.exports = {
+  getAllNames,
+  getConstellationsByQuadrant,
+};
+
